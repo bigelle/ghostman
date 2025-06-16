9a9350faa25913b7141ed27344ccc343216885d3
Bigeldinov Ruslan <ruslan.ok04@gmail.com>
2025-06-16 13:37:37 +0500

    feat: add fully serializable cookies

--- internal/httpcore/cookie.go
+++ internal/httpcore/cookie.go
@@ -2,8 +2,8 @@ package httpcore
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
-	"net/http"
 	"time"
 )
 
@@ -27,15 +27,78 @@ func (j CookieJar) Save(w io.Writer) error {
 }
 
 type Cookie struct {
-	Name        string        `json:"name"`
-	Value       string        `json:"value"`
-	Domain      string        `json:"domain,omitzero"`
-	Expires     time.Time     `json:"expires,omitzero"`
-	HttpOnly    bool          `json:"http_only,omitzero"`
-	MaxAge      int           `json:"max_age,omitzero"`
-	Partitioned bool          `json:"partitioned,omitzero"`
-	Path        string        `json:"path,omitzero"`
-	SameSite    http.SameSite `json:"same_site,omitzero"` // TODO: replace with my own for proper serialization
-	Secure      bool          `json:"secure,omitzero"`
+	Name        string     `json:"name"`
+	Value       string     `json:"value"`
+	Domain      string     `json:"domain,omitzero"`
+	Expires     CookieTime `json:"expires,omitzero"`
+	HttpOnly    bool       `json:"http_only,omitzero"`
+	MaxAge      int        `json:"max_age,omitzero"`
+	Partitioned bool       `json:"partitioned,omitzero"`
+	Path        string     `json:"path,omitzero"`
+	SameSite    SameSite   `json:"same_site,omitzero"`
+	Secure      bool       `json:"secure,omitzero"`
 }
 
+type CookieTime struct {
+	time.Time
+}
+
+func (c CookieTime) MarshalJSON() ([]byte, error) {
+	if c.IsZero() {
+		return []byte("null"), nil
+	}
+	return fmt.Appendf(nil, `"%s"`, c.In(time.FixedZone("GMT", 0)).Format(time.RFC1123)), nil
+}
+
+func (c *CookieTime) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		c.Time = time.Time{}
+		return nil
+	}
+
+	str := string(data[1 : len(data)-1])
+
+	t, err := time.Parse(time.RFC1123, str)
+	if err != nil {
+		return err
+	}
+
+	c.Time = t.In(time.FixedZone("GMT", 0))
+	return nil
+}
+
+type SameSite int
+
+const (
+	SameSiteDefaultMode SameSite = iota + 1
+	SameSiteLaxMode
+	SameSiteStrictMode
+	SameSiteNoneMode
+)
+
+func (s SameSite) String() string {
+	return [...]string{"", "Lax", "Strict", "None"}[s-1]
+}
+
+func (s SameSite) FromString(ss string) SameSite {
+	return map[string]SameSite{
+		"":       SameSiteDefaultMode,
+		"Lax":    SameSiteLaxMode,
+		"Strict": SameSiteStrictMode,
+		"None":   SameSiteNoneMode,
+	}[ss]
+}
+
+func (s SameSite) MarshalJSON() ([]byte, error) {
+	return json.Marshal(s.String())
+}
+
+func (s *SameSite) UnmarshalJSON(data []byte) error {
+	var i string
+	err := json.Unmarshal(data, &i)
+	if err != nil {
+		return nil
+	}
+	*s = s.FromString(i)
+	return nil
+}
---
+++ internal/httpcore/cookie_test.go
@@ -0,0 +1,34 @@
+package httpcore
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCookie_Marshal(t *testing.T) {
+	c := Cookie{
+		Name:        "foo",
+		Value:       "bar",
+		Domain:      "example.com",
+		Expires:     CookieTime{time.Date(2025, time.January, 1, 0, 0, 0, 0, time.FixedZone("GMT", 0))},
+		HttpOnly:    true,
+		MaxAge:      42,
+		Partitioned: true,
+		Path:        "/",
+		SameSite:    SameSiteLaxMode,
+		Secure:      true,
+	}
+	b, err := json.Marshal(c)
+	require.NoError(t, err)
+
+	var shouldBe Cookie
+	err = json.Unmarshal(b, &shouldBe)
+	require.NoError(t, err)
+	
+	assert.Equal(t, c, shouldBe)
+}
+
